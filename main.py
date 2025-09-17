e552174027b342f0fc3d5105c7f24de4089d2023
BiAnran-tku <613785079@o365.tku.edu.tw>
2025-09-17 06:13:53 +0000

    Finished 1st week

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ def main():
     print(f"CPU Usage: {psutil.cpu_percent(interval=1)}%")
     print(f"Memory Usage: {psutil.virtual_memory().percent}%")
     print(f"Disk Usage: {psutil.disk_usage('/').percent}%")
-
+    print(f"Complete")
 if __name__ == "__main__":
     main()
